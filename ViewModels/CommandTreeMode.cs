267735b9e76bcb5011ffef182ebacbdcdaff6fc1
jason.li <861342438@qq.com>
2026-07-22 17:28:36 +0800

    界面显示TreeView

---
+++ ViewModels/CommandTreeMode.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ProtocolSimulator.Models.DataType;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtocolSimulator.ViewModels
+{
+    public partial class CommandTreeMode : ObservableObject
+    {
+        public CommandTreeMode(string displayName,MBusDataType? command = null)
+        {
+            DisplayName = displayName;
+            Command = command;
+        }
+
+        public string DisplayName { get; }
+
+        MBusDataType? Command { get; }
+
+        public ObservableCollection<CommandTreeMode> Children { get; } = new ObservableCollection<CommandTreeMode>();
+
+        public bool IsCommand => Command.HasValue;
+
+        public string CodeText
+        {
+            get
+            {
+                if (!Command.HasValue)
+                {
+                    return string.Empty;
+                }
+
+                return $"0x{(ushort)Command.Value:X4}";
+            }
+        }
+
+        [ObservableProperty]
+        public bool _isEnable;
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Logging;
 using NLog;
 using ProtocolSimulator.Models;
+using ProtocolSimulator.Models.DataType;
 
 namespace ProtocolSimulator.ViewModels
 {
@@ -16,13 +18,39 @@ namespace ProtocolSimulator.ViewModels
         public ObservableCollection<LogItem> _logItems = new ObservableCollection<LogItem>();
 
         [ObservableProperty]
-        public ObservableCollection<str> _commandList = new();
+        public ObservableCollection<CommandTreeMode> _commandList = new();
+
+        [ObservableProperty]
+        public CommandTreeMode _selectedCommand;
 
         private readonly ILogger<MainWindowViewModel> _logger;
 
         public MainWindowViewModel(ILogger<MainWindowViewModel> logger)
         {
             _logger = logger;
+            CommandList = InitialCommandList();
+        }
+
+        private ObservableCollection<CommandTreeMode> InitialCommandList()
+        {
+            Dictionary<string, CommandTreeMode> result = new();
+
+            foreach (MBusDataType command in Enum.GetValues(typeof(MBusDataType)))
+            {
+                string[] commamdNode = command.ToString().Split("_");
+                string commandType = commamdNode[0];
+                string commandName = commamdNode[1];
+
+                if(!result.TryGetValue(commandType,out CommandTreeMode node))
+                {
+                    node = new CommandTreeMode(commandType);
+
+                    result.Add(commandType, node);
+                }
+
+                node.Children.Add(new CommandTreeMode(commandName, command));
+            }
+            return new ObservableCollection<CommandTreeMode>(result.Values);
         }
 
         [RelayCommand]
